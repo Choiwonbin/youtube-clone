5880a83cab6f4bee1cdeeb802634cd98b82c5ce0
Wonbin Choi <po03158@gmail.com>
2022-04-12 16:37:07 +0900

    3. request, response, middleware

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
+const handleHome = (req, res, next) => {
+    return res.send("I love Middleware");
+};
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge")
+}
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);

8f10909742853fd000dcfda3fcd0da32316990e9
Wonbin Choi <po03158@gmail.com>
2022-04-22 23:54:09 +0900

    6.6 More Practice POST

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { watch, getEdit, postEdit } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);  //(//d+): 숫자만 입력토록 하는 정규식(Regular Expressions)
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;

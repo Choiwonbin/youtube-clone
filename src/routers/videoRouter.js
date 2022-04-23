4f6b41f1ee90b9b360add34222e84590af184210
Wonbin Choi <po03158@gmail.com>
2022-04-23 19:37:00 +0900

    6.19 Create a Video, Watch a Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);  //(//d+): 숫자만 입력토록 하는 정규식(Regular Expressions)
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;

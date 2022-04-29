bae91dcf8b9f884789df04c8c059d6482e7338ab
Wonbin Choi <po03158@gmail.com>
2022-04-29 10:10:39 +0900

    7.15 MongoSotre, Cookie, Environment Varilbles

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./views/middlewares";
 import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -12,9 +13,13 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd()+"/src/views");
 app.use(logger);
-app.use(express.urlencoded({extetnded: true}));
+app.use(express.urlencoded({extended: true}));
+
 app.use(session({
-    secret: "Hello!"
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.DB_URL})
 }));
 app.use(localsMiddleware);
 app.use("/", globalRouter);

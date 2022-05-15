25f2492a2ab6208460a4d28acebad3fe71bf3b00
Wonbin Choi <po03158@gmail.com>
2022-05-15 22:15:18 +0900

    8.12 Video Owner

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async (req, res) => {
     const videos = await Video.find({});
@@ -6,7 +7,7 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
     const {id} = req.params;
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("owner");
     if (!video) {
         return res.render("404", {pageTitle:"Video not found."});  
     }
@@ -40,6 +41,7 @@ export const getUpload = (req, res) => {
 }
 
 export const postUpload = async (req, res) => {
+    const {user: {_id}} = req.session;
     const file = req.file;
     const {title, description, hashtags} = req.body;
     try {
@@ -47,6 +49,7 @@ export const postUpload = async (req, res) => {
             title,
             description,
             fileUrl: file.path,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags)
         });
         return res.redirect("/");
